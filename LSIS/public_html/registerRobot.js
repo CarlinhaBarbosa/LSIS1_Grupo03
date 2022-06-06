27d0d0239b5fd6f30a46d8ea292086edaf72996d
João Ferreira <1200638@isep.ipp.pt>
2022-06-06 17:21:24 +0100

    alteração 1Teste e index. Adição dos restantes ficheiros

---
+++ LSIS/public_html/registerRobot.js
@@ -0,0 +1,40 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/javascript.js to edit this template
+ */
+
+/* global fetch */
+
+function Enviar() {
+
+    var idEquipaInput = document.getElementById("idEquipaId");
+    var nomeInput = document.getElementById("nomeId");
+    var macAddressInput = document.getElementById("macAddressId");
+
+    if (isNaN(idEquipaInput.value) || idEquipaInput.value === "") {
+        alert("Insira um ID válido");
+    }
+
+    if (nomeInput.value === "") {
+        alert("Insira um nome válido");
+    }
+
+    let form = document.getElementById("registerRobotForm");
+    let formdata = new FormData(form);
+
+    fetch('/registarRobot', {
+        method: 'POST',
+        body: formdata
+    })
+            .then((res) => {
+                if (res.status === 200) {
+                    var infoPanel = document.getElementById("infoPanel");
+                    infoPanel.innerHTML = "<div>Robô inserido com sucesso!</div>";
+                    return res.json();
+                } else {
+                    infoPanel.innerHTML = "<div>Ocorreu um erro na criação do Robô!</div>";
+                    return res.json();
+                }
+            })
+            .catch((err) => console.log(err));
+}
