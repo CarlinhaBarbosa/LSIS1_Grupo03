c934feeff3c4cd1acaeb259dcee8e6ce93ee861d
João Ferreira <1200638@isep.ipp.pt>
2022-06-25 12:28:51 +0100

    alterações

--- LSIS1/src/main/java/Server/BotTelegram.java
+++ LSIS1/src/main/java/Server/BotTelegram.java
@@ -54,6 +54,7 @@ public final class BotTelegram extends TelegramLongPollingBot {
                 classificacaoRondaGeral(update);
                 System.out.println(msgRec);
             }
+            update.getMessage().getText();
         }
     }
 
